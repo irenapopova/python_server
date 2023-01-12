020b1b188d7307c0f302abce96fc357df948cf1f
Irena <irenejpopova6@gmail.com>
2023-01-12 14:37:50 +0100

    routing

--- server.py
+++ server.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
-#print(__name__)
+print(__name__)
 
-#print (url_for('static', filename='favicon.ico'))
 @app.route('/') #anytime the browser requests
 def my_home():
-    print (url_for('static', filename='favicon.ico'))
-    return render_template('./index.html')
+    
+    return render_template('index.html')
 
 @app.route('/about.html') #
 def about():
@@ -17,15 +16,6 @@ def about():
 def work():
     return render_template('work.html')
 
-
-@app.route('/favicon.ico') #
-def blog():
-    return ""
-
-@app.route('/blog') #s
-def blog3():
-    return "These are my thoughts on my blog"
-
-@app.route("/blog/2023/dogs") #
-def blog2():
-    return "This is my dog  "
+@app.route('/contact.html') #
+def contact():
+    return render_template('contact.html')
