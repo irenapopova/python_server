c333bcf1d9fba9207bc815757ccd2cd491fca4e8
Irena <irenejpopova6@gmail.com>
2023-01-12 14:40:44 +0100

    routing fixed

--- server.py
+++ server.py
@@ -5,17 +5,8 @@ print(__name__)
 
 @app.route('/') #anytime the browser requests
 def my_home():
-    
     return render_template('index.html')
 
-@app.route('/about.html') #
-def about():
-    return render_template('about.html')
-
-@app.route('/work.html') #
-def work():
-    return render_template('work.html')
-
-@app.route('/contact.html') #
-def contact():
-    return render_template('contact.html')
+@app.route('/<string:page_name>') 
+def html_page(page_name):
+    return render_template(page_name)
